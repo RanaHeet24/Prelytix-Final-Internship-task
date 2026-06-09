f5f156388c6dc4f5039dbee953e86b9369cc0df4
Rana Heet <ranaheet0001@gmail.com>
2026-06-09 11:43:59 +0530

    Delete Day-2 to 5/react-devboard directory

--- Day-2 to 5/react-devboard/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="text-center p-5 mt-10 bg-base-200">
-      <p>Made with React + Tailwind + DaisyUI</p>
-    </footer>
-  );
-}
-
-export default Footer;
